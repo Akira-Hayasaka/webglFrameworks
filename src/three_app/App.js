468b87f85873a346de2d0e5aac64087a5383c484
Akira Hayasaka <akira.hayasaka@gmail.com>
2020-12-08 15:38:00 +0900

    rtt on app.js

--- src/three_app/App.js
+++ src/three_app/App.js
@@ -3,6 +3,23 @@ import * as AA from "./lib/Includer";
 
 import Test from "./src/Test";
 
+const vert = `			
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`;
+
+const frag = `
+varying vec2 vUv;
+uniform sampler2D tDiffuse;
+void main() {
+   gl_FragColor = texture2D( tDiffuse, vUv );
+  //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`;
+
 class App {
   constructor() {
     var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -10,50 +27,52 @@ class App {
     this.cube = new THREE.Mesh(geometry, material);
     AA.Globals.SCENE.add(this.cube);
 
-    const linematerial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-    const points = [];
-    points.push(new THREE.Vector3(-1, 0, 0));
-    points.push(new THREE.Vector3(0, 1, 0));
-    points.push(new THREE.Vector3(1, 0, 0));
-
-    const linegeometry = new THREE.BufferGeometry().setFromPoints(points);
-    const line = new THREE.Line(linegeometry, linematerial);
-    AA.Globals.SCENE.add(line);
-
-    this.test = new Test();
-    AA.ev.add_listener("test", (arg) => {
-      console.log("got", arg.val);
+    this.tex = new THREE.WebGLRenderTarget(AA.Globals.APP_W, AA.Globals.APP_H, {
+      minFilter: THREE.LinearFilter,
+      magFilter: THREE.NearestFilter,
+      format: THREE.RGBFormat,
     });
+    this.mat_quad = new THREE.ShaderMaterial({
+      uniforms: { tDiffuse: { value: this.tex.texture } },
+      vertexShader: vert,
+      fragmentShader: frag,
+      depthWrite: false,
+    });
+    const geom_quad = new THREE.PlaneBufferGeometry(
+      AA.Globals.APP_W,
+      AA.Globals.APP_H
+    );
+    this.quad = new THREE.Mesh(geom_quad, this.mat_quad);
+    this.quad.position.z = -300;
+    this.scene = new THREE.Scene();
+    this.scene.add(this.quad);
   }
 
   update = () => {
     this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 0.01;
-    this.test.update();
   };
 
   draw = () => {
+    // AA.Globals.RENDERER.render(AA.Globals.SCENE, AA.Globals.CAMERA);
+    AA.Globals.RENDERER.setRenderTarget(this.tex);
+    AA.Globals.RENDERER.clear();
     AA.Globals.RENDERER.render(AA.Globals.SCENE, AA.Globals.CAMERA);
-    AA.s_log.draw_string("this is from app", 50, 200);
+    AA.Globals.RENDERER.setRenderTarget(null);
+    AA.Globals.RENDERER.clear();
+
+    AA.Globals.RENDERER.render(this.scene, AA.Globals.CAMERA);
   };
 
-  keypressed = (key) => {
-    console.log("keypressed", key.val);
-  };
+  keypressed = (key) => {};
+  keyreleased = (key) => {};
 
-  keyreleased = (key) => {
-    console.log("keyreleased", key.val);
-
-    if (key.val === " ") {
-      this.test.put_log();
-    }
-    if (key.val === "e") {
-      this.test.emit_event();
-    }
-  };
-
-  test;
   cube;
+
+  tex;
+  mat_quad;
+  quad;
+  scene;
 }
 
 export default App;
