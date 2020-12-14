127e011226e62f4501f6715451f82d66625e384b
Akira Hayasaka <akira.hayasaka@gmail.com>
2020-12-15 05:51:18 +0900

    cam 3d aspect

--- src/three_app/lib/graphic/Camera.js
+++ src/three_app/lib/graphic/Camera.js
@@ -1,5 +1,7 @@
 import { OrthographicCamera, PerspectiveCamera } from "three";
+import Constants from "../Constants";
 import Globals from "../Globals";
+import ev from "../util/Event";
 
 class Camera2d extends OrthographicCamera {
   constructor(
@@ -23,6 +25,11 @@ class Camera3d extends PerspectiveCamera {
   ) {
     super(fov, aspect, near, far);
     this.position.z = 5;
+
+    ev.add_listener(Constants.WINDOW_RESIZED, () => {
+      this.aspect = Globals.APP_W / Globals.APP_H;
+      this.updateProjectionMatrix();
+    });
   }
 }
 
